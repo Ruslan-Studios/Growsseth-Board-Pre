410a56bb63ce6ca4113a04276bf5f93518d8a4b1
Noel <leonardoulino2005@gmail.com>
2024-03-28 12:05:40 +0100

    Aggiornamento della grandezza, inizio sviluppo dell'UI, aggiunto scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,10 @@ import { invoke } from "@tauri-apps/api/tauri";
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './html/menu.component.html',
+  styleUrl: './css/menu.component.scss'
 })
+
 export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
+  
 }
