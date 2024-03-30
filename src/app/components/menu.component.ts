3749e7628a16a68911d5b60a423a154a92eb8b78
UomoProtone <protonic.studios.pj@gmail.com>
2024-03-30 02:01:34 +0100

    palle test

--- src/app/components/menu.component.ts
+++ src/app/components/menu.component.ts
@@ -4,27 +4,35 @@ import { RouterOutlet } from '@angular/router';
 import { invoke } from "@tauri-apps/api/tauri";
 import { app } from '@tauri-apps/api';
 
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './../html/menu.component.html',
-  styleUrl: './../css/menu.component.scss'
-})
+@Component
+(
+  {
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet],
+    templateUrl: './../html/menu.component.html',
+    styleUrl: './../css/menu.component.scss'
+  }
+)
 
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit
+{
 
   gameVersion: string = '';
 
-  ngOnInit() {
+  ngOnInit()
+  {
     this.getVersion();
   }
 
-  async getVersion() {
-    try {
+  async getVersion()
+  {
+    try
+    {
       const version = await app.getVersion();
       this.gameVersion = version;
-    } catch (error) {
+    } catch (error)
+    {
       console.error('Error getting app version:', error);
     }
   }
